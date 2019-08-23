ab62f0e7b7b9ccdaaf1175612ed8469a7a960b89
Pawan Kumar <pawnk1804@gmail.com>
2019-08-23 22:57:05 +0530

    V1

---
+++ Chapters/Chapter_1/1.1_Is_Unique.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+bool isUniqueChars(const string &str){
+		if (str.length() > 128){
+			return false;
+		}
+		vector<bool> char_set(128);
+		for (int i = 0; i < str.length(); i++){
+			int val = str[i];
+			if (char_set[val]){
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
+}
+int main(){
+    vector<string> words = {"abcde","hello"};
+    for(auto word : words)
+    {
+        cout << word << string(": ") << boolalpha <<isUniqueChars(word) << endl;
+    }
+    return 0;
+}
